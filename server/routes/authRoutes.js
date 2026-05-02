dd2e3f2a467d455c5f077900028c2fb036eb4488
Parth Aggarwal <161509040+ParthAggarwal04@users.noreply.github.com>
2026-05-03 02:17:32 +0530

    Update authRoutes.js

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const validator = require('validator');
-const User = require('../models/User.js');
+const User = require('../models/user');
 const protect = require('../middleware/auth');
 
 const router = express.Router();
